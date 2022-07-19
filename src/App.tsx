bba05b1f07992a75a32dff259d080f4c1c1994ed
Lucas Duarte <lucas.duarte@dasa.com.br>
2022-07-19 19:54:37 -0300

    feat: copy feedback

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import "./App.css";
 
+type IconItem = {
+  value: { render: () => ReactNode };
+  key: string;
+};
+
 function App() {
-  const [iconList, setIconList] = useState<any[]>([]);
+  const [iconList, setIconList] = useState<IconItem[]>([]);
+  const [iconsCopied, setIconsCopied] = useState<string[]>([]);
+
+  const onCopyIcon = (iconName: string) => {
+    navigator.clipboard.writeText(`<${iconName} />`);
+    setIconsCopied((prevIcons) => [...prevIcons, iconName]);
+
+    setTimeout(() => {
+      setIconsCopied((prevIcons) =>
+        prevIcons.filter((icon) => icon !== iconName)
+      );
+    }, 3000);
+  };
 
   useEffect(() => {
     import("@dasa-health/alma-icons").then((module) => {
       const { ...iconsModules } = module as any;
       console.log({ iconsModules });
 
-      const icons = Object.entries(iconsModules).map(([key, value]) => ({
-        key,
-        value,
-      }));
+      const icons = Object.entries(iconsModules).map(
+        ([key, value]) =>
+          ({
+            key,
+            value,
+          } as IconItem)
+      );
       setIconList(icons);
     });
   }, []);
@@ -23,10 +43,8 @@ function App() {
         <div key={icon.key} className="card">
           <h2>{icon.key}</h2>
           <i>{icon.value.render()}</i>
-          <button
-            onClick={() => navigator.clipboard.writeText(`<${icon.key} />`)}
-          >
-            Copy component
+          <button onClick={() => onCopyIcon(icon.key)}>
+            {iconsCopied.includes(icon.key) ? "Icon copied!" : "Copy icon"}
           </button>
         </div>
       ))}
